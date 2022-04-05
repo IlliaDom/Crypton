011d74ab7b642b1d78498cecf32dc59684fbf4fe
Illia Domaratskyi <illia_domaratskyi@sp-lutsk.com>
2022-04-05 23:44:02 +0300

    fix key

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten:{
-      url: 'https://eth-ropsten.alchemyapi.io/v2/79S4ZADImA-DkoUGdRX0A990IXRXxqIG',
-      accounts: ['76bb2524cc9699f036c8bd26d3c87c3f63773b736b5c3a7b3c0ef4b71aa03ae8']
+      url: 'URL',
+      accounts: ['SECRET_KEY']
     }
   } 
 }
